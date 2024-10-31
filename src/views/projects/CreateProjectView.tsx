51f13eef8bc1483c4c1a1c65eed65a6356c453e7
Fernando <fernando.inversionmf@gmail.com>
2024-10-30 19:57:23 -0700

    Creating the form to create projects

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,21 +1,52 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 export default function CreateProjectView() {
+  const initialValues = {
+    projectName: "",
+    clientName: "",
+    description: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const handleForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-      <h1 className="text-5xl font-black">Create Project</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        Fill out the following form to create a project
-      </p>
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-black">Create Project</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Fill out the following form to create a project
+        </p>
 
-      <nav className="my-5">
-        <Link
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to="/"
+        <nav className="my-5">
+          <Link
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            to="/"
+          >
+            Back to Projects
+          </Link>
+        </nav>
+        <form
+          className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
+          action=""
         >
-          Back to Projects
-        </Link>
-      </nav>
+          <input
+            type="submit"
+            value="Create Project"
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+          />
+        </form>
+      </div>
     </>
   );
 }

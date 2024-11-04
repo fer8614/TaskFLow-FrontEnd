4fb07f5c9d52cd191dc3fc835bfb8c9abb14bc62
Fernando <fernando.inversionmf@gmail.com>
2024-11-03 21:20:02 -0800

    Get the Project to edit

--- src/views/projects/EditProjectView.tsx
+++ src/views/projects/EditProjectView.tsx
@@ -1,9 +1,18 @@
-import { useLinkClickHandler, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjectById } from "@/api/ProjectAPI";
 
 export default function EditProjectView() {
   const params = useParams();
   const projectId = params.projectId!;
-  console.log("projectId", projectId);
+
+  const { data, isLoading, error, isError } = useQuery({
+    queryKey: ["editProject", projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false,
+  });
+
+  console.log(data);
 
   return (
     <div>

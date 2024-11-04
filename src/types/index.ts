a7a173b214edeec95cee282ff3cfdbc13ca65083
Fernando <fernando.inversionmf@gmail.com>
2024-11-03 18:50:22 -0800

    Validating API response with Zod

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,21 @@
 import { z } from "zod";
 
 /** Projects */
-export const Project = z.object({
+export const projectSchema = z.object({
   _id: z.string(),
   projectName: z.string(),
   clientName: z.string(),
   description: z.string(),
 });
-
-export type Project = z.infer<typeof Project>;
+export const dashboardProjectSchema = z.array(
+  projectSchema.pick({
+    _id: true,
+    projectName: true,
+    clientName: true,
+    description: true,
+  }),
+);
+export type Project = z.infer<typeof projectSchema>;
 export type ProjectFormData = Pick<
   Project,
   "projectName" | "clientName" | "description"
--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -1,21 +1,42 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getProjects } from "@/api/ProjectAPI";
 
 export default function DashboardView() {
-  return (
-    <>
-      <h1 className="text-5xl font-black">My Projects</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        Manage and administer your projects
-      </p>
+  const { data, isLoading } = useQuery({
+    queryKey: ["projects"],
+    queryFn: getProjects,
+  });
 
-      <nav className="my-5">
-        <Link
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
-          to="/projects/create"
-        >
-          New Project
-        </Link>
-      </nav>
-    </>
-  );
+  if (isLoading) return "Loading...";
+
+  if (data)
+    return (
+      <>
+        <h1 className="text-5xl font-black">My Projects</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Manage and administer your projects
+        </p>
+
+        <nav className="my-5">
+          <Link
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+            to="/projects/create"
+          >
+            New Project
+          </Link>
+        </nav>
+
+        {data.length ? (
+          <p>There are Projects</p>
+        ) : (
+          <p className="text-center py-20">
+            There are no projects yet{" "}
+            <Link to="/projects/create" className="text-fuchsia-500 font-bold">
+              Create a new project
+            </Link>
+          </p>
+        )}
+      </>
+    );
 }

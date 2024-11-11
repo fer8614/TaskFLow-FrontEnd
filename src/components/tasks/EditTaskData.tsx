a34c70c02fd5f33db668e81e1174e239ddd7fd45
Fernando <fernando.inversionmf@gmail.com>
2024-11-10 20:33:46 -0800

    Query the API with the task to edit

--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,6 +1,7 @@
 import { useLocation, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getTaskById } from "@/api/TaskAPI";
+import EditTaskModal from "./EditTaskModal";
 
 export default function EditTaskData() {
   const params = useParams();
@@ -13,9 +14,8 @@ export default function EditTaskData() {
   const { data } = useQuery({
     queryKey: ["task", editTaskId],
     queryFn: () => getTaskById({ projectId, taskId: editTaskId }),
+    enabled: !!editTaskId,
   });
 
-  console.log(data);
-
-  return <div>EditTaskData</div>;
+  if (data) return <EditTaskModal />;
 }

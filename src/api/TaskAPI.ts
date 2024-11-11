f1d18447fc053531c45a62a0af9394588ddaaa9b
Fernando <fernando.inversionmf@gmail.com>
2024-11-10 20:20:46 -0800

    Getting the Task to edit

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,10 +1,11 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { Project, TaskFormData } from "../types";
+import { Project, Task, TaskFormData } from "../types";
 
 type TaskAPI = {
   formData: TaskFormData;
   projectId: Project["_id"];
+  taskId: Task["_id"];
 };
 export async function createTask({
   formData,
@@ -15,7 +16,22 @@ export async function createTask({
     const { data } = await api.post<string>(url, formData);
     return data;
   } catch (error) {
-    if (isAxiosError(error)) {
+    if (isAxiosError(error) && error.response) {
+      console.log("error", error.response?.data.error);
+    }
+  }
+}
+
+export async function getTaskById({
+  projectId,
+  taskId,
+}: Pick<TaskAPI, "projectId" | "taskId">) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}`;
+    const { data } = await api(url);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
       console.log("error", error.response?.data.error);
     }
   }
--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,9 +1,21 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getTaskById } from "@/api/TaskAPI";
 
 export default function EditTaskData() {
+  const params = useParams();
+  const projectId = params.projectId!;
+
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const editTaskId = queryParams.get("editTask");
-  console.log(editTaskId);
+  const editTaskId = queryParams.get("editTask")!;
+
+  const { data } = useQuery({
+    queryKey: ["task", editTaskId],
+    queryFn: () => getTaskById({ projectId, taskId: editTaskId }),
+  });
+
+  console.log(data);
+
   return <div>EditTaskData</div>;
 }

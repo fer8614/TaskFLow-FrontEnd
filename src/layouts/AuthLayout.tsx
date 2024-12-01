ec6accbb7748bc301c07ecb665fcfb03bf3683c3
Fernando <fernando.inversionmf@gmail.com>
2024-11-30 19:26:02 -0800

    Creating the Function to Register the Account

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/components/logo";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 export default function AuthLayout() {
   return (
@@ -7,12 +8,12 @@ export default function AuthLayout() {
       <div className="bg-gray-800 min-h-screen">
         <div className="py-10 lg:py-20 mx-auto w-[450px]">
           <Logo />
-
           <div className="mt-10">
             <Outlet />
           </div>
         </div>
       </div>
+      <ToastContainer pauseOnHover={false} pauseOnFocusLoss={false} />
     </>
   );
 }

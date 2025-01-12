f2b13c687c98ba24f89c21e5f4218473c29b0010
Fernando <fernando.inversionmf@gmail.com>
2025-01-11 17:15:40 -0800

    Displaying the search result for a user to add to the project

--- src/components/team/AddMemberForm.tsx
+++ src/components/team/AddMemberForm.tsx
@@ -4,6 +4,7 @@ import { useMutation } from "@tanstack/react-query";
 import ErrorMessage from "../ErrorMessage";
 import { TeamMemberForm } from "@/types/index";
 import { findUserByEmail } from "@/api/TeamAPI";
+import SearchResult from "./SearchResult";
 
 export default function AddMemberForm() {
   const initialValues: TeamMemberForm = {
@@ -66,6 +67,7 @@ export default function AddMemberForm() {
         {mutation.error && (
           <p className="text-center">{mutation.error.message}</p>
         )}
+        {mutation.data && <SearchResult user={mutation.data.data} />}
       </div>
     </>
   );

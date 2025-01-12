c1145d9f5aac0b0e55f1360615e921c4c8f1838b
Fernando <fernando.inversionmf@gmail.com>
2025-01-11 19:27:14 -0800

    get and display team members

--- src/api/TeamAPI.ts
+++ src/api/TeamAPI.ts
@@ -1,6 +1,11 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { Project, TeamMember, TeamMemberForm } from "../types";
+import {
+  Project,
+  TeamMember,
+  TeamMemberForm,
+  teamMembersSchema,
+} from "../types";
 
 export async function findUserByEmail({
   projectId,
@@ -31,6 +36,21 @@ export async function addUserToProject({
     const url = `/projects/${projectId}/team`;
     const { data } = await api.post(url, { id });
     return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getProjectTeam(projectId: Project["_id"]) {
+  try {
+    const url = `/projects/${projectId}/team`;
+    const { data } = await api(url);
+    const response = teamMembersSchema.safeParse(data);
+    if (response.success) {
+      return response.data;
+    }
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error);

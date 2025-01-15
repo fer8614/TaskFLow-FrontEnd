26024b2546e9d15f9e7ced0cceec74bef518e2a0
Fernando <fernando.inversionmf@gmail.com>
2025-01-14 20:14:44 -0800

    Creating a history of changes to tasks

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -30,6 +30,7 @@ export async function getTaskById({
   try {
     const url = `/projects/${projectId}/tasks/${taskId}`;
     const { data } = await api(url);
+    return data;
     const response = taskSchema.safeParse(data);
     if (response.success) {
       return response.data;

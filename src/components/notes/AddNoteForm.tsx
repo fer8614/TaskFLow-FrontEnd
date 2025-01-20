f71e8805f6abc88da851490855784a519dbc906e
Fernando <fernando.inversionmf@gmail.com>
2025-01-19 17:32:40 -0800

    Creating the type in Zod for creating notes in tasks

--- src/components/notes/AddNoteForm.tsx
+++ src/components/notes/AddNoteForm.tsx
@@ -1,6 +1,28 @@
+import { useForm } from "react-hook-form";
+import { NoteFormData } from "@/types/index";
+import ErrorMessage from "../ErrorMessage";
+
 export default function AddNoteForm() {
+  const initialValues: NoteFormData = {
+    content: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+
+  const handleAddNote = (formData: NoteFormData) => {
+    console.log(formData);
+  };
+
   return (
-    <form onSubmit={() => {}} className="space-y-3" noValidate>
+    <form
+      onSubmit={handleSubmit(handleAddNote)}
+      className="space-y-3"
+      noValidate
+    >
       <div>
         <label className="font-bold" htmlFor="content">
           Create Note
@@ -10,7 +32,13 @@ export default function AddNoteForm() {
           type="text"
           placeholder="Content of the note"
           className="w-full p-3 border border-gray-300"
+          {...register("content", {
+            required: "Content is required",
+          })}
         />
+        {errors.content && (
+          <ErrorMessage>{errors.content.message}</ErrorMessage>
+        )}
       </div>
 
       <input
--- src/types/index.ts
+++ src/types/index.ts
@@ -33,6 +33,17 @@ export const userSchema = authSchema
 
 export type User = z.infer<typeof userSchema>;
 
+/** Notes */
+export const noteSchema = z.object({
+  _id: z.string(),
+  content: z.string(),
+  createdAt: userSchema,
+  task: z.string(),
+});
+
+export type Note = z.infer<typeof noteSchema>;
+export type NoteFormData = Pick<Note, "content">;
+
 /** Tasks */
 export const taskStatusSchema = z.enum([
   "pending",

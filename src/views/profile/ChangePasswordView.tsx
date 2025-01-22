01ddec4065bba0d62996dc4bacaea1abccfc256e
Fernando <fernando.inversionmf@gmail.com>
2025-01-21 20:55:07 -0800

    Creating the function to change the password from React

--- src/views/profile/ChangePasswordView.tsx
+++ src/views/profile/ChangePasswordView.tsx
@@ -1,3 +1,160 @@
+import { useForm } from "react-hook-form";
+import ErrorMessage from "@/components/ErrorMessage";
+import { UpdateCurrentUserPasswordForm } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { ChangePassword } from "@/api/ProfileAPI";
+import { toast } from "react-toastify";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
 export default function ChangePasswordView() {
-  return <div>ChangePasswordView</div>;
+  const initialValues: UpdateCurrentUserPasswordForm = {
+    current_password: "",
+    password: "",
+    password_confirmation: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+  const { mutate } = useMutation({
+    mutationFn: ChangePassword,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => toast.success(data),
+  });
+  const password = watch("password");
+
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleChangePassword = (formData: UpdateCurrentUserPasswordForm) => {
+    mutate(formData);
+  };
+
+  return (
+    <>
+      <div className="mx-auto max-w-3xl">
+        <h1 className="text-5xl font-black ">Change Password</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          Use this form to change your password
+        </p>
+
+        <form
+          onSubmit={handleSubmit(handleChangePassword)}
+          className=" mt-14 space-y-5 bg-white shadow-lg p-10 rounded-lg"
+          noValidate
+        >
+          <div className="mb-5 space-y-3">
+            <label
+              className="text-sm uppercase font-bold"
+              htmlFor="current_password"
+            >
+              Current Password
+            </label>
+            <div className="relative">
+              <input
+                id="current_password"
+                type={showCurrentPassword ? "text" : "password"}
+                placeholder="Current Password"
+                className="w-full p-3 border border-gray-200"
+                {...register("current_password", {
+                  required: "The current password is required",
+                })}
+              />
+              <button
+                type="button"
+                onClick={() => setShowCurrentPassword(!showCurrentPassword)}
+                className="absolute inset-y-0 right-0 p-3"
+              >
+                <FontAwesomeIcon
+                  icon={showCurrentPassword ? faEyeSlash : faEye}
+                />
+              </button>
+            </div>
+            {errors.current_password && (
+              <ErrorMessage>{errors.current_password.message}</ErrorMessage>
+            )}
+          </div>
+
+          <div className="mb-5 space-y-3">
+            <label className="text-sm uppercase font-bold" htmlFor="password">
+              New Password
+            </label>
+            <div className="relative">
+              <input
+                id="password"
+                type={showNewPassword ? "text" : "password"}
+                placeholder="New Password"
+                className="w-full p-3 border border-gray-200"
+                {...register("password", {
+                  required: "New Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "The password must be at least 8 characters long",
+                  },
+                })}
+              />
+              <button
+                type="button"
+                onClick={() => setShowNewPassword(!showNewPassword)}
+                className="absolute inset-y-0 right-0 p-3"
+              >
+                <FontAwesomeIcon icon={showNewPassword ? faEyeSlash : faEye} />
+              </button>
+            </div>
+            {errors.password && (
+              <ErrorMessage>{errors.password.message}</ErrorMessage>
+            )}
+          </div>
+
+          <div className="mb-5 space-y-3">
+            <label
+              htmlFor="password_confirmation"
+              className="text-sm uppercase font-bold"
+            >
+              Repeat Password
+            </label>
+            <div className="relative">
+              <input
+                id="password_confirmation"
+                type={showConfirmPassword ? "text" : "password"}
+                placeholder="Repeat Password"
+                className="w-full p-3 border border-gray-200"
+                {...register("password_confirmation", {
+                  required: "This field is required",
+                  validate: (value) =>
+                    value === password || "Passwords are not equal",
+                })}
+              />
+              <button
+                type="button"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="absolute inset-y-0 right-0 p-3"
+              >
+                <FontAwesomeIcon
+                  icon={showConfirmPassword ? faEyeSlash : faEye}
+                />
+              </button>
+            </div>
+            {errors.password_confirmation && (
+              <ErrorMessage>
+                {errors.password_confirmation.message}
+              </ErrorMessage>
+            )}
+          </div>
+
+          <input
+            type="submit"
+            value="Change Password"
+            className="bg-fuchsia-600 w-full p-3 text-white uppercase font-bold hover:bg-fuchsia-700 cursor-pointer transition-colors"
+          />
+        </form>
+      </div>
+    </>
+  );
 }
